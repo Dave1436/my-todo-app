fb24fccee2d0996fd728093d6b9399f21e37500c
davec <davechapman68@googlemail.com>
2023-01-17 18:57:05 +0000

    Initial Commit

--- web.py
+++ web.py
@@ -11,8 +11,7 @@ def add_todo():
     st.session_state["new_todo"] = ""
 
 st.title("My Todo App")
-st.subheader("This is my todo app.")
-st.write("This app is to increase your productivity")
+st.subheader("What's today's focus?")
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
